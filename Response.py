3f1b567380aef3cf7cfd0f906868adab6e317964
Sam Clark <sam.clark1142@gmail.com>
2019-09-24 19:42:16 -0400

    responses

--- Response.py
+++ Response.py
@@ -11,9 +11,53 @@ class Response:
 
     @property
     def message(self):
-        return self._message.encode("ascii")
+        message_ = self._message + "\n"
+        return message_.encode("ascii")
 
 
 class Ack(Response):
     def _message(self):
         return "HELLO"
+
+
+class AuthYes(Response):
+    def _message(self):
+        return "AUTHYES"
+
+
+class AuthNo(Response):
+    def _message(self):
+        return "AUTHNO"
+
+
+class SignIn(Response):
+    def __init__(self, user):
+        self.user = user
+
+    def _message(self):
+        return "SIGNIN:" + self.user
+
+
+class SignOff(Response):
+    def __init__(self, user):
+        self.user = user
+
+    def _message(self):
+        return "SIGNOFF:" + self.user
+
+
+class UserList(Response):
+    def __init__(self, clients):
+        self.clients = clients
+
+    def _message(self):
+        return ", ".join([client.username for client in self.clients])
+
+
+class UserMessage(Response):
+    def __init__(self, from_client, message):
+        self.from_client = from_client
+        self.user_message = message
+
+    def _message(self):
+        return "From:" + self.from_client + ":" + self.user_message
